0fbbda48baaf8093bdc6b70cf6af7d1d401e6fb1
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-05-18 08:34:36 +0530

    model schema

--- controllers/job.js
+++ controllers/job.js
@@ -1,33 +1,49 @@
 const jobModel = require("../model/job");
 
 const createJobs = async (req, res) => {
-  const jobObj = req.body;
-  const newJob = new jobModel(jobObj);
-  await newJob.save();
-  res.json({
-    success: true,
-    message: "Job added succesful",
-  });
+  try {
+    const jobObj = req.body;
+    const newJob = new jobModel(jobObj);
+    await newJob.save();
+    res.json({
+      success: true,
+      message: "Job added succesful",
+      jobId: newlySavedJob._id,
+    });
+  } catch {
+    res.json({
+      success: false,
+      message: "Something went wrong, please try again after sometime",
+    });
+  }
 };
 
 const listJobs = async (req, res) => {
-  const {minSalary, maxSalary} = req.query;
+
+  try{
+  const { minSalary, maxSalary } = req.query;
   const jobsList = await jobModel.find({
-    $and:[{salary :{$gte:minSalary}}]
+    $and: [{ salary: { $gte: minSalary } }, { salary: { $lte: maxSalary } }],
   });
 
   res.json({
-    success:true,
-    message:"list job api",
-    result: jobsList
-  })
+    success: true,
+    message: "list job api",
+    result: jobsList,
+  });
+  }
+  catch{
+    res.json({
+      success: false,
+      message: "Something went wrong, please try again after sometime",
+    });
+  }
 };
 
 const editJobs = async (req, res) => {
-  
   const id = req.parms.id;
   console.log(id);
-  jobModel.findByIdAndUpdate(id,)
+  jobModel.findByIdAndUpdate(id);
   res.json({
     success: true,
     message: "Dummy edit",
--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const app = express();
-
 const jobRoutes = require("./routes/job");
 
+const app = express();
 app.use(express.json());
-app.use(jobRoutes)
 
-// moongose
+//----------- moongose-----------------------------------
 mongoose
-.connect("mongodb://localhost:27017/jobApp")
+.connect("mongodb+srv://sanketm457:A24BoRgX2tyAHyOz@jobapp.ezdevqu.mongodb.net/")
 .then(()=>console.log("Connection done"))
-.catch((err)=>console.log("error"+err))
+.catch((err)=>console.log("Error in connection"+err))
 
-//routes
+//--------------routes-----------------------------------
 
 app.get('/',(req,res)=>{
     res.send("helelo")
 })
 
+app.use(jobRoutes)
+
 app.listen(3002,()=>{
     console.log("Server Started");
 })
 
+
+
+//----------------other-------------------------
+//mongodb+srv://sanketm457:A24BoRgX2tyAHyOz@jobapp.ezdevqu.mongodb.net/
+// sanketm457
