15968d65b8e2b7321604d1c6af28689c0edbd3d6
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-05-20 20:24:29 +0530

    env file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const jobRoutes = require("./routes/job");
+const dotenv = require('dotenv');
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
 //----------- moongose-----------------------------------
 mongoose
-.connect("mongodb+srv://sanketm457:A24BoRgX2tyAHyOz@jobapp.ezdevqu.mongodb.net/")
+.connect(process.env.CONNECTION_KEY)
 // .connect("mongodb://localhost:27017/jobApp")
 .then(()=>console.log("Connection done"))
 .catch((err)=>console.log("Error in connection"+err))
